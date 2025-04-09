5393c30bd91d47d3115f4f6234f5f7d65e4c7fdd
Mariell <mariell_ham@dlsl.edu.ph>
2025-04-09 21:46:49 +0800

    upddd

--- traveldiaryApp/App.tsx
+++ traveldiaryApp/App.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './src/screens/Home';
-import CameraScreen from './src/Camera';
-import GeolocationScreen from './src/Geolocation';
-import AsyncStorageTest from './src/AsyncStorage';
-import NotificationScreen from './src/LocalPushNotification';
-import { GlobalContextProvider } from './src/screens/GlobalContext';
-
-const Stack = createStackNavigator();
+import { GlobalContextProvider } from './src/context/GlobalContext';
+import AppNavigator from './src/navigation/AppNavigator'; 
 
 export default function App() {
   return (
     <GlobalContextProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Camera" component={CameraScreen} />
-          <Stack.Screen name="Geolocation" component={GeolocationScreen} />
-          <Stack.Screen name="AsyncStorage" component={AsyncStorageTest} />
-          <Stack.Screen name="Notifications" component={NotificationScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator />
     </GlobalContextProvider>
   );
 }
