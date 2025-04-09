e3a43c076177285e7cbcaa2a5fd8276179f6958a
Mariell <mariell_ham@dlsl.edu.ph>
2025-04-09 14:07:22 +0800

    initial stuff

---
+++ traveldiaryApp/App.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import CameraScreen from './src/Camera';
+import GeolocationScreen from './src/Geolocation';
+import NotificationScreen from './src/LocalPushNotification';
+import AsyncStorageTest from './src/AsyncStorage';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CameraScreen></CameraScreen>
+      <GeolocationScreen></GeolocationScreen> 
+      <NotificationScreen></NotificationScreen> 
+      <AsyncStorageTest></AsyncStorageTest> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: StatusBar.currentHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
