1525b8e95657d15492f2cf1874790cd03eedcf0f
Mariell <mariell_ham@dlsl.edu.ph>
2025-04-11 23:42:14 +0800

    fff

--- traveldiaryApp/App.tsx
+++ traveldiaryApp/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { GlobalContextProvider } from './src/context/GlobalContext';
 import AppNavigator from './src/navigation/AppNavigator'; 
+import * as Notifications from 'expo-notifications';
+
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
   }),
 });
 
